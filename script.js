e61f369176b1df33b84687ea9e1120e96d9fe752
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-28 17:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,48 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to fetch data from an API URL and measure the time taken
+function fetchDataWithTime(url) {
+  return new Promise((resolve, reject) => {
+    const startTime = performance.now();
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const endTime = performance.now();
+        const timeTaken = endTime - startTime;
+        resolve(timeTaken);
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+// Function to fetch data using Promise.all() and display the result
+function fetchWithPromiseAll() {
+  Promise.all(apiUrls.map(fetchDataWithTime))
+    .then((times) => {
+      const sumTime = times.reduce((total, time) => total + time, 0);
+      const avgTime = sumTime / times.length;
+      const outputDiv = document.getElementById("output-all");
+      outputDiv.textContent = `${avgTime.toFixed(2)} ms`;
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    });
+}
+
+// Function to fetch data using Promise.any() and display the result
+function fetchWithPromiseAny() {
+  Promise.any(apiUrls.map(fetchDataWithTime))
+    .then((time) => {
+      const outputDiv = document.getElementById("output-any");
+      outputDiv.textContent = `${time.toFixed(2)} ms`;
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    });
+}
+
+// Call the functions to compare the performance
+fetchWithPromiseAll();
+fetchWithPromiseAny();
+
